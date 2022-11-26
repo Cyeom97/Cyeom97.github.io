e3cb380cf9d5356847d4e684362278ff885446a1
Christopher Yeom <chrisyeom97@gmail.com>
2022-11-25 23:32:07 -0500

    added css and home page

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
