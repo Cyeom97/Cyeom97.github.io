2f48929b6c827e4e79a72e778d661b9e59380ba9
Christopher Yeom <chrisyeom97@gmail.com>
2023-08-27 15:52:39 -0400

    final installations

--- yeom/src/App.js
+++ yeom/src/App.js
@@ -1,7 +1,18 @@
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <div>Hi</div>
+    </div>
+  )
 }
 
 export default App
